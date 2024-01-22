6516d9294ac27eedcb4f886b5c92d654975413f2
gregcox1979 <30574786+gregcox1979@users.noreply.github.com>
2024-01-22 16:38:45 +0000

    Update 3_dailyopsfromtop.js

--- 3_dailyopsfromtop.js
+++ 3_dailyopsfromtop.js
@@ -1,3 +1,5 @@
+var runLabel = "RUN5"
+
 //mongosh-pt1
 function csvdumpdoc(doc) {
     let o = ""
@@ -21,6 +23,7 @@ collections = ["closelot",
     "taxlot",
     "taxlotEffect",
     "accrualEffect",
+    "accrualSnapshot",
     "deal",
     "rerate",
     "pnlIncSnapshot",
@@ -45,29 +48,26 @@ collections = ["closelot",
 
 //mongosh-pt2 - change as required
 shards = [
-    'shard1_661','shard2_1478','shard3_1479','shard4_1480','shard5_1481','shard6_1482','shard7_1100','shard8_1101','shard9_1103','shard10_660',
+    'shard1_661','shard2_1478','shard3_1479','shard4_1480','shard5_1481','shard6_1482','shard7_5166','shard8_5167','shard9_5168','shard10_660',
     'shard11_694','shard12_662','shard13_663','shard14_664','shard15_665','shard16_666','shard17_667','shard18_668','shard19_696','shard20_697',
     'shard21_1483','shard22_1484','shard23_1485','shard24_1535','shard25_1536','shard26_1537','shard27_3540','shard28_3541','shard29_3535','shard30_3536',
     'shard31_3537','shard32_3538','shard33_3560','shard34_3561','shard35_3562','shard36_3857','shard37_3858','shard38_3859','shard39_3860','shard40_3861',
     'shard41_3862','shard42_3863','shard43_3864','shard44_3865','shard45_3866','shard46_3867','shard47_3868','shard48_4290','shard49_4291','shard50_4260',
     'shard51_4261','shard52_4262','shard53_4263','shard54_4268','shard55_4269','shard56_4270','shard57_4288','shard58_4271','shard59_4272','shard60_4273',
-    'shard61_4274','shard62_5041','shard63_5042','shard64_5043','shard65_5038'
+    'shard61_4274','shard62_5041','shard63_5042','shard64_5043','shard65_5038','shard66_4321','shard67_4322','shard68_4583','shard69_4584','shard70_4602',
+    'shard71_4603','shard72_4604','shard73_4605','shard74_4606','shard75_4607','shard76_4608','shard77_4609'
 ]
 
 //mongosh-pt3 
+db = new Mongo().getDB("sem");
 for (sh of shards) {
     var shard = 'shard_output/' + sh
     var fileName = shard+'_TOP_SUMMARY.txt';
+    var shard_no = sh
 
     filenames = [
-        [shard+"/RUN1_HK_START_TOP.js",shard+"/RUN1_HK_END_TOP.js"],
-        [shard+"/RUN2_HK_START_TOP.js",shard+"/RUN2_HK_END_TOP.js"],    
-        [shard+"/RUN3_HK_START_TOP.js",shard+"/RUN3_HK_END_TOP.js"],
-        [shard+"/RUN4_HK_START_TOP.js",shard+"/RUN4_HK_END_TOP.js"],
-        [shard+"/RUN5_HK_START_TOP.js",shard+"/RUN5_HK_END_TOP.js"],
-        [shard+"/RUN6_HK_START_TOP.js",shard+"/RUN6_HK_END_TOP.js"],
-        [shard+"/RUN7_HK_START_TOP.js",shard+"/RUN7_HK_END_TOP.js"],
-        [shard+"/RUN8_HK_START_TOP.js",shard+"/RUN8_HK_END_TOP.js"],
+        [shard+"/RUN5_HK_EXT_TOP_START.js",shard+"/RUN5_HK_EXT_TOP_END.js"]
+        // [shard+"/RUN22_HK_START_TOP.js",shard+"/RUN2_HK_END_TOP.js"]
     ]
 
     fs.writeFile(fileName, "TOP Comparisons for Shard: "+shard+"\n", (err) => {
@@ -83,9 +83,11 @@ for (sh of shards) {
         let end = JSON.parse(fileTwo);
         for (ns of collections) {
                 print(sh +' - '+fn+ ' - ' +i)
+                collection = ns
                 ns = `sem.${ns}`
                 obj = start.totals[ns];
                 rec = { ns }
+                rec['shard'] = shard_no
 
                 if (i==0) {
                     fs.appendFile(fileName, "\n" + fn[0] + " : " + fn[1] + "\n", (err) => {
@@ -98,15 +100,17 @@ for (sh of shards) {
                     endData = end.totals[ns][op]
                     rec[op] = endData.count - startData.count
                 }
+                rec['file'] = fn
+                rec['run'] = runLabel   
+                //print(rec)
                 
                 var data = csvdumpdoc(rec);
                 fs.appendFile(fileName, i+' ->'+data, function (err) {
                     if (err) throw err;
                 });
-                
+                db.getCollection(collection).insertOne(rec)    
                 i++;
             }
 
         }
 }
-
